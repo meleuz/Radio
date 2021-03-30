92ce264d7b3d151eb0b3f4d439dd8b39e1c7a14a
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-30 21:09:20 +0500

    Radio added

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class Radio {
+    int currentStation;
+    int currentVolume;
+    final int minStation = 0;
+    int numberStation = 10;
+    int maxStation = 9;
+    final int maxVolume = 100;
+    final int minVolume = 0;
+}
