fdc39cd58711ea16befd38422e7e39601b7bd90f
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-31 19:45:39 +0500

    RadioTest added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,8 +1,8 @@
 package ru.netology.domain;
 public class Radio {
     private int maxRadioWave = 9;
-    private int minRadioWave = 0;
-    private int currentRadioWave = 0;
+    private int minRadioWave;
+    private int currentRadioWave;
     private int maxSoundLevel = 10;
     private int minSoundLevel;
     private int currentSoundLevel;
@@ -22,7 +22,7 @@ public class Radio {
     public int getCurrentRadioWave() {
         return currentRadioWave;
     }
-    public int setCurrentRadioWave(int currentRadioWave) {
+    public void setCurrentRadioWave(int currentRadioWave) {
         if (currentRadioWave > maxRadioWave) {
             currentRadioWave = minRadioWave;
         }
@@ -30,15 +30,20 @@ public class Radio {
             currentRadioWave = maxRadioWave;
         }
         this.currentRadioWave = currentRadioWave;
-        return currentRadioWave;
     }
+
     public int currentRadioWaveUp() {
-        return setCurrentRadioWave(currentRadioWave + 1);
+        setCurrentRadioWave(currentRadioWave);
+        return ++currentRadioWave;
     }
+
     public int currentRadioWaveDown() {
-        return setCurrentRadioWave(currentRadioWave - 1);
+        setCurrentRadioWave(currentRadioWave);
+        return --currentRadioWave;
     }
-    // Radio sound levels
+
+// Radio sound levels
+
     public int getMaxSoundLevel() {
         return maxSoundLevel;
     }
@@ -54,26 +59,20 @@ public class Radio {
     public int getCurrentSoundLevel() {
         return currentSoundLevel;
     }
-
-
-        public int setCurrentSoundLevel(int currentSoundLevel) {
+        public void setCurrentSoundLevelUp(int currentSoundLevel) {
+            currentSoundLevel++;
             if (currentSoundLevel > maxSoundLevel) {
                 currentSoundLevel = maxSoundLevel;
             }
+            this.currentSoundLevel = currentSoundLevel;
+        }
 
+        public void setCurrentSoundLevelDown(int currentSoundLevel) {
+            currentSoundLevel--;
             if (currentSoundLevel < minSoundLevel) {
                 currentSoundLevel = minSoundLevel;
             }
             this.currentSoundLevel = currentSoundLevel;
-            return currentSoundLevel;
-        }
-
-        public int currentSoundLevelUp() {
-            return setCurrentSoundLevel(currentSoundLevel + 1);
-        }
-
-        public int currentSoundLevelDown() {
-            return setCurrentSoundLevel(currentSoundLevel - 1);
         }
 
     }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,51 +1,52 @@
 package ru.netology.domain;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class RadioTest {
     Radio radio = new Radio();
+
     @Test
     public void validateRadioWaveOverPossible() {
-        radio.setCurrentRadioWave(18);
         assertEquals(0, radio.getCurrentRadioWave());
-    }
-    @Test
-    public void validateRadioWaveLessPossible() {
-        radio.setCurrentRadioWave(-1);
-        assertEquals(9, radio.getCurrentRadioWave());
-    }
-    @Test
-    public void currentRadioWaveUp() {
-        radio.setCurrentRadioWave(9);
-        assertEquals(0, radio.currentRadioWaveUp());
-    }
-    @Test
-    public void currentRadioWaveDown() {
-        radio.setCurrentRadioWave(0);
-        assertEquals(9, radio.currentRadioWaveDown());
+        radio.setCurrentRadioWave(10);
+        assertEquals(0, radio.getCurrentRadioWave());
     }
 
     @Test
+    public void validateRadioWaveLessPossible() {
+        assertEquals(0, radio.getCurrentRadioWave());
+        radio.setCurrentRadioWave(-1);
+        assertEquals(9, radio.getCurrentRadioWave());
+    }
+
+    @Test
+    public void currentRadioWaveUp() {
+        assertEquals(0, radio.getCurrentRadioWave());
+        radio.setCurrentRadioWave(5);
+        assertEquals(6, radio.currentRadioWaveUp());
+    }
+
+    @Test
+    public void currentRadioWaveDown() {
+        assertEquals(0, radio.getCurrentRadioWave());
+        radio.setCurrentRadioWave(3);
+        assertEquals(2, radio.currentRadioWaveDown());
+    }
+
+// ----------------------------------
+
+    @Test
     public void validateChangeSoundLevelOverPossible() {
-        radio.setCurrentSoundLevel(15);
+        assertEquals(0, radio.getCurrentSoundLevel());
+        radio.setCurrentSoundLevelUp(15);
         assertEquals(10, radio.getCurrentSoundLevel());
     }
 
     @Test
     public void validateChangeSoundLevelUnderPossible() {
-        radio.setCurrentSoundLevel(-8);
         assertEquals(0, radio.getCurrentSoundLevel());
-    }
-
-    @Test
-    public void currentSoundLevelUp() {
-        radio.setCurrentSoundLevel(10);
-        assertEquals(10, radio.currentSoundLevelUp());
-    }
-
-    @Test
-    public void currentSoundLevelDown() {
-        radio.setCurrentSoundLevel(0);
-        assertEquals(0, radio.currentSoundLevelDown());
+        radio.setCurrentSoundLevelDown(-8);
+        assertEquals(0, radio.getCurrentSoundLevel());
     }
 
 }
